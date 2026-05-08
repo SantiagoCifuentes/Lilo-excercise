e183d2338d2d06e71299e82e7251075e8e2f9917
santcial <santiago.ci9619@gmail.com>
2026-05-07 22:29:29 -0500

    added tsconfig.ts  to  enable  .env variables

---
+++ tests/happy-path.spec.ts
@@ -0,0 +1,12 @@
+import {test, expect} from '@playwright/test';
+import 'dotenv/config';
+
+test('should login successfully with valid credentials', async ({page}) => {
+  // Navigate to the login page
+  await page.goto(process.env.BASE_URL!);
+  
+  
+  
+  
+  
+  ;});
